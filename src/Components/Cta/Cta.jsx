1ddb3697a649f62c855ec5c42e99500f464fff9a
Hrushikesh Pathak <hrushikeshpathak@Hrushikeshs-MacBook-Pro.local>
2024-09-24 16:46:55 +0530

    finalised

--- src/Components/Cta/Cta.jsx
+++ src/Components/Cta/Cta.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Cta.css'
-
+import { Link } from 'react-router-dom'
 const Cta = ({ bg, link, col, stroke }) => {
     return (
         <div className=''>
-        <a href={link} className="cta" style={{ '--cta-bg-color': bg}}>
+        <Link to={link} className="cta" style={{ '--cta-bg-color': bg}}>
             <span style={{ color: col }}>Read More</span>
             <svg width="15px" height="10px" viewBox="0 0 13 10">
                 <path d="M1,5 L11,5" stroke={stroke}></path>
                 <polyline points="8 1 12 5 8 9" stroke={stroke}></polyline>
             </svg>
-        </a>
+        </Link>
         </div>
     )
 }
